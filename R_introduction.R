5a425d89e96663ffb01149dcd8255b96ac20dbb4
taewan625 <taewan1997625@gmail.com>
2022-05-04 18:56:35 +0900

    Create R_introduction.R

---
+++ R_introduction.R
@@ -0,0 +1,9 @@
+
+# https://www.edwith.org/ptnr/kobic
+
+# R은 이미 많은 사람들이 관련 package를 구성해 놓았으므로 우리는 필요한 package를 찾고 
+# package의 데이터 객체(object)와 함수(Function)를 어떻게 사용하는지만 알면 유용하게 사용 가능
+# 각자 분야에 따른 분석 pakage들이 달라서 package를 많이 배우는 것보다 
+# 처음 접하는 package라도 package의 메뉴얼과 r의 help system, 구글링 스스로 할수있는 사전지식을 갖추는 것이 중요
+
+#  Bioconductor -> install -> 
